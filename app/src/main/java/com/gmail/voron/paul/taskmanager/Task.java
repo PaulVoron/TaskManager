f8d5efc7a94d9c55df536082cd7069a29deaf266
Pavlo Voronin <voron.paul@gmail.com>
2019-03-31 17:51:41 +0300

    Home Task 03

---
+++ app/src/main/java/com/gmail/voron/paul/taskmanager/Task.java
@@ -0,0 +1,19 @@
+package com.gmail.voron.paul.taskmanager;
+
+public class Task {
+    private final String name;
+    private final int priority;
+
+    public Task(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
